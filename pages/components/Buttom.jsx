8fa1239752cb1ae574ecda0466e9735797b3d075
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2023-01-19 23:50:38 +0900

    build

--- pages/components/Buttom.jsx
+++ pages/components/Buttom.jsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { title } from "process";
 
 const Buttom = ({ title, link, bgcolor, fontcolor }) => {
   return (
--- pages/components/MideaButtom.jsx
+++ pages/components/MideaButtom.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Link from "next/link";
 import AppleLogo from "./AppleLogo";
 import GoogleLogo from "./GoogleLogo";
