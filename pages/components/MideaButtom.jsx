48a4f94231ff069c7faa5befe8b2ca7ac70fd13b
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2023-01-20 00:12:03 +0900

    link cor

--- pages/components/MideaButtom.jsx
+++ pages/components/MideaButtom.jsx
@@ -1,19 +1,19 @@
-// import Link from "next/link";
-// import AppleLogo from "./AppleLogo";
-// import GoogleLogo from "./GoogleLogo";
+import Link from "next/link";
+import AppleLogo from "./AppleLogo";
+import GoogleLogo from "./GoogleLogo";
 
 const MideaButtom = ({ title, link, bgcolor, fontcolor, logo }) => {
   return (
     <div
       className={`w-[300px] my-5 py-3 m-auto flex items-center justify-center ${bgcolor} ${fontcolor} text-center rounded-xl shadow-md`}
     >
-      {/* {logo ? (
+      {logo ? (
         <GoogleLogo className=" float-left mr-4" />
       ) : (
         <AppleLogo className=" float-left mr-4" />
-      )} */}
+      )}
 
-      {/* <Link href={link}>{title}</Link> */}
+      <Link href={link ?? "/"}>{title}</Link>
     </div>
   );
 };
