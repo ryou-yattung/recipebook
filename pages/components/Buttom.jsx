d98c8b96d6477fb9a7b63b5348d1fc765674dd9d
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2023-01-20 00:08:03 +0900

    link cor

--- pages/components/Buttom.jsx
+++ pages/components/Buttom.jsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
+// import Link from "next/link";
 
 const Buttom = ({ title, link, bgcolor, fontcolor }) => {
   return (
     <div
       className={`w-[300px] my-5 py-3 m-auto ${bgcolor} ${fontcolor} text-center rounded-xl shadow-md`}
     >
-      <Link href={link}>{title}</Link>
+      {/* <Link href={link}>{title}</Link> */}
     </div>
   );
 };
