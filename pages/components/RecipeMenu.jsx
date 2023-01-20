0d7f45729ee855d46073a97b3315fa98f33e722a
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2023-01-20 11:10:06 +0900

    push

--- pages/components/RecipeMenu.jsx
+++ pages/components/RecipeMenu.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+// import Image from "next/image";
 
 const RecipeMenu = () => {
   return (
     <div>
-      <Image src={Record} alt="Record" />
+      {/* <Image src={Record} alt="Record" /> */}
       <p>レシート履歴</p>
     </div>
   );
