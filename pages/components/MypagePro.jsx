41b509622ecbd4e5d882d103081191dd3c5240fd
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2023-01-26 15:38:35 +0900

    push

---
+++ pages/components/MypagePro.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import mypageIcon from "../assets/mypagePng.png";
+import mypageLv from "../assets/mypageLv.png";
+import mypageBg from "../assets/mypageBg.png";
+
+const MypagePro = () => {
+  return (
+    <section className="w-screen h-36">
+      <Image
+        src={mypageBg}
+        alt="mypageBg"
+        className="w-full h-auto absolute z-10"
+      />
+
+      <div className="w-[90%] relative z-50 flex justify-between m-auto">
+        <div className="flex items-center pt-5">
+          <Image src={mypageIcon} alt="icon" />
+          <div className="ml-4 text-fontColor font-bold">
+            <h2 className="text-2xl">つむぎ様</h2>
+            <p className="text-[12px] mt-1">会員番号：09876543</p>
+          </div>
+        </div>
+
+        <div className="">
+          <Image src={mypageLv} alt="mypageLv" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MypagePro;
--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,11 +1,46 @@
 import React from "react";
 import HpFooterPart from "./components/parts/HpFooterPart";
 import HpHeader from "./components/parts/HpHeader";
+import MypagePro from "./components/MypagePro";
+import SmileLoge from "./components/SmileLogo";
+import MailLogo from "./components/MailLogo";
+import TicketLogo from "./components/TicketLogo";
+import RankingLogo from "./components/RankingLogo";
 
 const mypage = () => {
   return (
     <div className="w-screen h-screen relative z-10 py-[105px] bg-bgColor">
       <HpHeader title="マイページ" />
+
+      <MypagePro />
+
+      <section className="flex items-center justify-around text-sm font-bold text-fontColor my-10">
+        <div className="w-18 text-center">
+          <SmileLoge className="m-auto mb-4" />
+          <p>会員情報</p>
+        </div>
+
+        <div className="w-18 text-center">
+          <MailLogo className="m-auto mb-4" />
+          <p>お知らせ</p>
+        </div>
+
+        <div className="w-18 text-center">
+          <TicketLogo className="m-auto mb-4" />
+          <p>クーポン履歴</p>
+        </div>
+
+        <div className="w-18 text-center">
+          <RankingLogo className="m-auto mb-4" />
+          <p>会員ランク</p>
+        </div>
+      </section>
+
+      <section>
+        <h2 className="text-xl font-bold text-fontColor text-center border-b-2 border-greenDark2 pb-2">
+          設定
+        </h2>
+      </section>
 
       <HpFooterPart
         homeColor={false}
